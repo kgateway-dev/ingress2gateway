a4ac991e00ca1ccc631a2bb0c5ee375eb0eb3053
shivansh-source <shivanshsiddhi1234@gmail.com>
2026-04-02 01:27:45 +0530

    wrong file it should be in print.go
    Signed-off-by: shivansh-source <shivanshsiddhi1234@gmail.com>

--- cmd/print.go
+++ cmd/print.go
@@ -35,6 +35,7 @@ import (
 
 	// Call init function for the providers
 	_ "github.com/kgateway-dev/ingress2gateway/pkg/i2gw/providers/ingressnginx"
+	_ "github.com/kgateway-dev/ingress2gateway/pkg/i2gw/providers/glooedge"
 
 	// Call init for emitters
 	_ "github.com/kgateway-dev/ingress2gateway/pkg/i2gw/emitters/agentgateway"
